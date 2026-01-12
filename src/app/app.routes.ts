206d348495ca950a5f03e09d9668d47f6b7a8e56
diegocardonahernandez <diego.cardona@tajamar365.com>
2026-01-12 13:36:21 +0100

    Interfaz Perfil Usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Principal } from './components/principal/principal';
 import { InicioSesion } from './components/inicio-sesion/inicio-sesion';
 import { Home } from './components/home/home';
 import { Menu } from './components/menu/menu';
+import { Perfil } from './components/perfil/perfil';
 
 export const routes: Routes = [
   // { path: '', component: InicioSesion },
@@ -17,6 +18,9 @@ export const routes: Routes = [
       { path: 'home', component: Home },
     ],
   },
+  //Pongo las las rutas de menu y perfil para irlas viendo mientras las diseño
+  //Ya el miercoles organizamos el router.
+  {path: 'perfil', component: Perfil},
   {path: "menu", component: Menu},
   { path: '**', redirectTo: '' },
 ];
