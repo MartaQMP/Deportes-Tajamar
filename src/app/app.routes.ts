bc9d70d2459ef6453db031456f060ebb35fc0a1d
MartaQMP <martaquirosmp@gmail.com>
2026-01-14 18:44:20 +0100

    Rutas terminadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,20 +5,15 @@ import { Menu } from './components/menu/menu';
 import { Perfil } from './components/perfil/perfil';
 
 export const routes: Routes = [
-  // { path: '', component: InicioSesion },
-  {path: '', component :InicioSesion},
+  { path: '', component: InicioSesion },
   {
-    path: 'principal',
+    path: 'deportes',
     component: Menu,
-    /*CUANDO SE HAGAN LAS PAGINAS DE HOME Y DEMAS DEBEN SER HIJOS DE
-    LA PRINCIPAL QUE ES DONDE ESTARA LA LLAMADA AL MENU Y ROUTER-OUTLET*/
-
     children: [
-      {path: 'home', component: Home },
-      {path: 'perfil', component: Perfil},
+      { path: '', component: Home },
+      { path: 'perfil', component: Perfil },
+      { path: '**', redirectTo: '' },
     ],
   },
-  //Pongo las las rutas de menu y perfil para irlas viendo mientras las diseño
-  //Ya el miercoles organizamos el router.
   { path: '**', redirectTo: '' },
 ];
