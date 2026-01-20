c7637d43cb3d4e013e4d68b4c56590b7390ef05a
MartaQMP <martaquirosmp@gmail.com>
2026-01-20 20:08:08 +0100

    Cambiadad ruta para q vaya a componente equipos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
       { path: 'perfil', component: Perfil },
       { path: 'cursos', component: Cursos },
       { path: 'resultados', component: Resultados },
-      { path: 'crear-equipo/:ideventoactividad', component: Resultados },
+      { path: 'crear-equipo/:ideventoactividad', component: Equipos },
       { path: 'solicitar-material/:idEvento', component: SolicitarMaterial },
       { path: '**', redirectTo: '' },
     ],
